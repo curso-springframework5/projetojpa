03bf574e475e925656cbb7bb430c8f65cf8bf53a
ayrtonperrut <ayrton01perrut@gmail.com>
2020-07-13 18:42:38 -0300

    first commit

---
+++ src/main/java/com/testedbh2/TesteDbH2Application.java
@@ -0,0 +1,13 @@
+package com.testedbh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDbH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDbH2Application.class, args);
+	}
+
+}
